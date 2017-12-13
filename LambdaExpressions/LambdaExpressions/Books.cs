aa48dab6008e0ad943be1545dbe1c764b3949b6e
Anu Tomy <anuatomy@gmail.com>
2017-12-13 11:02:58 -0500

    Example of Lambda Expressions

---
+++ LambdaExpressions/LambdaExpressions/Books.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LambdaExpressions
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
