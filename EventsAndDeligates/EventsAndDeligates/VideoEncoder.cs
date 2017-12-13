8623021521a8821296195fbd19fdbfc1edcc874c
Anu Tomy <anuatomy@gmail.com>
2017-12-13 14:56:17 -0500

    Example of Events and Deligates

---
+++ EventsAndDeligates/EventsAndDeligates/VideoEncoder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+namespace EventsAndDeligates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+    public class VideoEncoder
+    {
+        // 1- Define a delegate
+        // 2- Define an event based on that delegate
+        // 3- Raise the event
+
+/*Define a delegate*/
+//public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+//public event VideoEncodedEventHandler VideoEncoded;
+
+//Define an event based on that delegate
+public event EventHandler<VideoEventArgs> VideoEncoded;
+
+public void Encode(Video video)
+{
+    Console.WriteLine("Encoding video...");
+    Thread.Sleep(3000);
+
+    OnVideoEncoded(video);
+}
+
+//Raise the event
+protected virtual void OnVideoEncoded(Video video)
+{
+    if (VideoEncoded != null)
+        VideoEncoded(this, new VideoEventArgs() { Video = video });
+}
+}
+}
