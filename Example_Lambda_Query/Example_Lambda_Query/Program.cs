b0a7a86aa6e4287b0e643a6e7a615afd757cd9ad
Anu Tomy <anuatomy@gmail.com>
2017-12-13 11:19:34 -0500

    Example of using Lambda Expressions in a Query

---
+++ Example_Lambda_Query/Example_Lambda_Query/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_Lambda_Query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data source.
+            int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+            
+            int highScoreCount = scores.Where(n => n > 80).Count();
+
+            Console.WriteLine("{0} scores are greater than 80", highScoreCount);
+              
+        }
+    }
+}
+
+// Outputs: 4 scores are greater than 80  
