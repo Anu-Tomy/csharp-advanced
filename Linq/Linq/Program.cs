183d326b8a5c96f0262a46853c6447dfa7672810
Anu Tomy <anuatomy@gmail.com>
2017-12-13 23:00:55 -0500

    Example of Linq

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            //LINQ Query Operators
+            var cheaperBooks =
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b;
+
+            //LINQ Extension Methods
+            var cheapBooks = books
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            foreach (var book in cheapBooks)
+                Console.WriteLine(book);
+        }
+    }
+}
